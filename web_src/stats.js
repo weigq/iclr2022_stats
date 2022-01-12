8c91dd76f64668bb6b981f16138f0643a1dcb5ae
weigq <weigq1234@gmail.com>
2022-01-12 22:46:13 +0800

    ✅: clean code

--- web_src/stats.js
+++ web_src/stats.js
@@ -1,2 +1,1 @@
-
-new Tablesort(document.getElementById('all-subs'));
+function scrollFunction(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}new Tablesort(document.getElementById("all-subs")),mybutton=document.getElementById("myBtn"),window.onscroll=function(){scrollFunction()};
